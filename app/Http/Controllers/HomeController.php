10a0f3037423e69b826a620eda286fd690d9a99e
Emmanuel Oduro <emmarthurson@gmail.com>
2020-03-18 19:32:08 +0000

    hotfix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Service;
+use App\Models\Attendance;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
@@ -24,21 +25,47 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index(Request $request)
+    public function index(Request $request, $count = 1)
     {
         $user = $request->user();
 
-        $service = Service::latest()->first();
+        $service = Service::whereBetween('created_at', [
+                now()->copy()->startOfDay()->toDateTimeString(),
+                now()->copy()->endOfDay()->toDateTimeString(),
+        ])->first();
 
-        $cache_service = 'service' . $user->id;
 
+        // $cache_service = 'service' . $user->id;
+
+        // if($service){
+        //     views($service)->cooldown(60)->record();
+        //     if (!(Cache::has($cache_service))) {
+        //         Cache::store('file')->put('service.' . $user->id, $service->id, 600);
+        //     }
+        // }
+
+      
         if($service){
-            views($service)->cooldown(60)->record();
-            if (!(Cache::has($cache_service))) {
-                Cache::store('file')->put('service.' . $user->id, $service->id, 600);
+            $check_attendance = Attendance::where('user_id', $user->id)
+            ->where('service_id', $service->id)
+            ->whereBetween('created_at', [
+                now()->copy()->startOfDay()->toDateTimeString(),
+                now()->copy()->endOfDay()->toDateTimeString(),
+            ])->first();
+
+            if($check_attendance){
+                if($check_attendance->count == $count){
+                    $check_attendance->count = $count;
+                    // $check_attendance->count = $count;
+                }
+            }else{
+                $attendance = new Attendance;
+                $attendance->user_id = $user->id;
+                $attendance->service_id = $request->service;
+                $attendance->count = $count;
+                $attendance->save();
             }
         }
-
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -33,9 +33,9 @@ Route::middleware(['auth'])->group(function(){
     Route::get('views', function(){
         $service = Service::latest()->first();
 
-        $service = views($service)->count();
+        // $service = views::with()->get();
 
-        return $service;
+        return $service->views;
     });
 
 });
