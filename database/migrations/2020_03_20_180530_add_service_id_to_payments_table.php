fccb78ad71c6c2afa9425264ca23389f706c965e
Emmanuel Oduro <emmarthurson@gmail.com>
2020-03-20 18:07:39 +0000

    hotfix

---
+++ database/migrations/2020_03_20_180530_add_service_id_to_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddServiceIdToPaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->unsignedbiginteger('service_id')->nullable();
+            $table->foreign('service_id')->references('id')->on('services');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->dropColumn('service_id');
+            $table->dropColumn('service_id');
+        });
+    }
+}
