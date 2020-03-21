cfadc081e6d64d7171e26fe08acce84461a6fd9d
Emmanuel Oduro <emmarthurson@gmail.com>
2020-03-21 18:25:45 +0000

    hotfix

---
+++ database/migrations/2020_03_21_182513_add_user_id_to_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdToPaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->unsignedbiginteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('payments', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+            $table->dropColumn('user_id');
+        });
+    }
+}
