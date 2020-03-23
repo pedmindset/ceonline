89867c0e9725ed169d13263df080dd32667da784
Emmanuel Oduro <emmarthurson@gmail.com>
2020-03-23 14:09:28 +0000

    hotfix

---
+++ database/migrations/2020_03_22_084005_create_salvations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalvationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salvations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedbiginteger('church_id')->nullable();
+            $table->foreign('church_id')->references('id')->on('churches');
+            $table->unsignedbiginteger('service_id')->nullable();
+            $table->foreign('service_id')->references('id')->on('services');
+            $table->unsignedbiginteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('first_timers');
+    }
+}
