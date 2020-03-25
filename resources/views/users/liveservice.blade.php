7bbedaaa93c1f1c24ed9ba45dd9dfeeed2c65fd6
Emmanuel Oduro <emmarthurson@gmail.com>
2020-03-25 15:58:13 +0000

    hotfix

--- resources/views/users/liveservice.blade.php
+++ resources/views/users/liveservice.blade.php
@@ -206,15 +206,13 @@
             <p class="p-1 px-3 py-3 tex-gray-700 hidden lg:block text-2xl"> Previous Videos </p>
             @forelse ($services as $s)
             <a href="../videos/{{ $s->id }}">
-                <div class="h-auto md:max-w-xs  md:min-w-0 min-w-18 my-3 py-5 bg-indigo-500 rounded-lg shadow-2xl flex flex-col text-white items-center mx-2">
+                <p class=" truncate h-auto md:max-w-xs  md:min-w-0 min-w-18 my-3 py-5 bg-indigo-500 rounded-lg shadow-2xl flex flex-col text-white items-center mx-2">
                     <span class="rounded-full bg-white">
                         <i class=" text-indigo-400 las la-play text-3xl p-3"></i>
                     </span>
-                    <div>
-                    <p class="text-lg mt-2 px-2 font-bold px-4 max-w-xl truncate">{{ $s->title }}</p>
-                    </div>
-                    <time class="text-sm">{{ $s->created_at->diffForHumans() }}</time>
-                </div>
+                    <p class="text-lg mt-2 font-bold px-4 max-w-xl text-center">{{ $s->title }}</p>
+                    <p class="text-sm text-center">{{ $s->created_at->diffForHumans() }}</p>
+                  </p>
             </a>
             @empty
             <p class="p-4">No Videos. Thank you</p>
