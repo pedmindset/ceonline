73fd94d6bc888c4ae8d25a0c417a4c29c7d04600
Emmanuel Oduro <emmarthurson@gmail.com>
2020-03-29 07:36:16 +0000

    hotfix

--- public/js/websocket.js
+++ public/js/websocket.js
@@ -10043,7 +10043,8 @@ window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/d
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
   key: "1314244",
-  cluster: "mt1",
+  wsHost: window.location.hostname,
+  wsPort: 6001,
   encrypted: false
 });
 
