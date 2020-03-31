1188176446892adb7b9e48309bb39b732c63c1c6
Emmanuel Oduro <emmarthurson@gmail.com>
2020-03-31 12:00:52 +0000

    hotfix

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -7,7 +7,7 @@
       <p class="hidden sm:block"></p>
       <div>
         <button v-on:click.prevent="first_timer()"   type="button" class="inline-flex md:text-base text-sm  justify-center rounded-md border border-transparent px-4 py-2 bg-red-600 leading-6 font-medium text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-          <i class="las la-handshake text-2xl mr-1"></i> Click here, if this is your first time worshipping with us!
+          <i class="las la-handshake text-2xl mr-1"></i> Click here, if this is your first time in Christ Embassy!
         </button>
       </div>
     </div>
