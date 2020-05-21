65621ce4053ab804ef201a1a57ed9645e05637d2
Emmanuel Oduro <emmarthurson@gmail.com>
2020-05-21 12:30:28 +0000

    EVENT

---
+++ app/Models/Event.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use CyrildeWit\EloquentViewable\InteractsWithViews;
+use CyrildeWit\EloquentViewable\Contracts\Viewable;
+use Illuminate\Support\Str;
+
+class Event extends Model
+{
+    /**
+    * @var  string
+    */
+    protected $table = 'events';
+
+    protected $casts = [
+    'start_date' => 'datetime',
+    'end_date' => 'datetime',
+    'created_at' => 'datetime',
+    'updated_at' => 'datetime',
+    ];
+
+       /**
+     * Boot function from laravel.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->slug = (string) Str::slug($model->title .'-'. time());
+            $model->uuid = Str::uuid();
+        });
+
+        // static::updating(function ($model) {
+        //     $model->slug = (string) Str::slug($model->title ."-". time());
+        // });
+    }
+
+
+
+    public function church()
+    {
+        return $this->belongsTo('App\Models\Church', 'church_id', 'id');
+    }
+
+
+    public function users()
+    {
+        return $this->belongsToMany('App\Models\User');
+    }
+
+
+    public function services()
+    {
+        return $this->belongsToMany('App\Models\Service');
+    }
+
+}
