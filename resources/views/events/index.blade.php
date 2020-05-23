d4a200c3ec69dc82ef97ba84b6335d4d6c9bbb40
Emmanuel Oduro <emmarthurson@gmail.com>
2020-05-23 15:08:34 +0000

    hotfix

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -111,7 +111,7 @@
 
                         <div class="text-right sm:col-span-2">
                             <span class="inline-flex rounded-md shadow-sm">
-                                <button class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out" >
+                                <button v-on:click="submitRegistration" class="inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out" >
                                     Register
                                 </button>
                             </span>
