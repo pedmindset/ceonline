8637df578b01379c84c9daba6ce1b9d87edc653c
Emmanuel Oduro <emmarthurson@gmail.com>
2020-05-24 08:37:49 +0000

    hotfix

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -50,7 +50,7 @@
                   <p class="mt-4 text-lg leading-7 text-gray-500 sm:mt-3">
                         {!! $event->description !!}
                   </p>
-                  <div  v-if="registered == false" class="mt-9 grid grid-cols-1 row-gap-6 sm:grid-cols-2 sm:col-gap-8">
+                     <div  v-if="registered == false" class="mt-9 grid grid-cols-1 row-gap-6 sm:grid-cols-2 sm:col-gap-8">
                         <div class="sm:col-span-2">
                             <label for="country" class="block text-sm font-medium leading-5 text-gray-700">
                             Title
@@ -111,37 +111,38 @@
                             </span>
                         </div>
                     </div>
-                  </div>
-                  <div v-else>
-                    <div class="rounded-md bg-green-50 p-4">
-                        <div class="flex">
-                          <div class="flex-shrink-0">
-                            <svg class="h-5 w-5 text-green-400" fill="currentColor" viewBox="0 0 20 20">
-                              <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
-                            </svg>
-                          </div>
-                          <div class="ml-3">
-                            <h3 class="text-sm leading-5 font-medium text-green-800">
-                              Already Registered
-                            </h3>
-                            <div class="mt-2 text-sm leading-5 text-green-700">
-                              <p>
-                               Dear @{{ user.name }}, You are registered for @{{ event.title }}
-                              </p>
-                            </div>
-                            <div class="mt-4">
-                              <div class="-mx-2 -my-1.5 flex">
-                              <a href="{{ url('/') }}">
-                                <button class="px-2 py-1.5 rounded-md text-sm leading-5 font-medium text-green-800 hover:bg-green-100 focus:outline-none focus:bg-green-100 transition ease-in-out duration-150">
-                                  Go Home
-                                </button>
-                              </a>
+                    <div v-else>
+                        <div class="rounded-md bg-green-50 p-4">
+                            <div class="flex">
+                              <div class="flex-shrink-0">
+                                <svg class="h-5 w-5 text-green-400" fill="currentColor" viewBox="0 0 20 20">
+                                  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
+                                </svg>
+                              </div>
+                              <div class="ml-3">
+                                <h3 class="text-sm leading-5 font-medium text-green-800">
+                                  Already Registered
+                                </h3>
+                                <div class="mt-2 text-sm leading-5 text-green-700">
+                                  <p>
+                                   Dear @{{ user.name }}, You are registered for @{{ event.title }}
+                                  </p>
+                                </div>
+                                <div class="mt-4">
+                                  <div class="-mx-2 -my-1.5 flex">
+                                  <a href="{{ url('/') }}">
+                                    <button class="px-2 py-1.5 rounded-md text-sm leading-5 font-medium text-green-800 hover:bg-green-100 focus:outline-none focus:bg-green-100 transition ease-in-out duration-150">
+                                      Go Home
+                                    </button>
+                                  </a>
+                                  </div>
+                                </div>
                               </div>
                             </div>
                           </div>
-                        </div>
                       </div>
                   </div>
+
 
               </div>
             </div>
