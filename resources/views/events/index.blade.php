0f8bd7fa6ca6ad9ae5dc06d90c4beae4e58cc396
Emmanuel Oduro <emmarthurson@gmail.com>
2020-05-24 12:32:48 +0000

    hotfix

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -111,7 +111,7 @@
                             </span>
                         </div>
                     </div>
-                    <div v-else>
+                    <div v-else class="mt-9 grid grid-cols-1 row-gap-6 sm:grid-cols-2 sm:col-gap-8">
                         <div class="rounded-md bg-green-50 p-4 mt-10">
                             <div class="flex">
                               <div class="flex-shrink-0">
@@ -226,7 +226,7 @@
                                    'icon': 'success'
                                })
 
-                                window.location.href = window.location.host;
+                                window.location.href = {{ url('/')}};
                             }).catch(function(e){
                                 console.log(e);
 
